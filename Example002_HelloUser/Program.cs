ca865977d1c45c73bd3ce013ccc2a0aea5610a26
Galina <akaides@yandex.ru>
2022-12-17 19:17:15 +1000

    add Example 002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите свое имя ");
+string username = Console.ReadLine(); //string-строка (тип данных); username-имя переменной
+Console.Write("Привет, ");
+Console.Write(username);
